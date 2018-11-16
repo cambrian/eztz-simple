5157b404af7ff718327000507a8d9c7dc05b0108
Sanjay Kannan <whaatt@gmail.com>
2018-11-16 12:02:02 -0500

    Make timeout configurable.

--- src/eztz-simple.ts
+++ src/eztz-simple.ts
@@ -18,6 +18,7 @@ interface TransferCommand {
   toPKH: eztz.PublicKeyHash,
   amount: eztz.MuTez,
   fee: eztz.MuTez
+  timeout: number
 }
 
 program
@@ -37,26 +38,32 @@ program
   .option('-t, --toPKH <hash>', 'receiver public key hash')
   .option('-a, --amount <value>', 'amount to transfer in mutez')
   .option('-p, --fee <value>', 'desired operation fee in mutez')
+  .option('-m, --timeout [sec]', 'timeout in seconds for request', parseInt)
   .action(async (options: TransferCommand) => {
     if (!options.node) die('No Tezos node provided.')
     if (!options.fromSK) die('No sender SK provided.')
     if (!options.toPKH) die('No receiver PKH provided.')
     if (!options.amount) die('No amount provided.')
     if (!options.fee) die('No fee provided.')
+    if (!options.timeout) {
+      console.log('Using default timeout (5 seconds).')
+      options.timeout = 5
+    }
     try {
       eztz.node.setProvider(options.node)
       const keys = eztz.crypto.extractKeys(options.fromSK)
       const tezAmount = eztz.utility.totez(options.amount)
       const tezFee = eztz.utility.totez(options.fee)
-      // Manually timeout the transfer since default behavior is to retry for a while on a bad URI.
-      await timeout(eztz.rpc.transfer(keys.pkh, keys, options.toPKH, tezAmount, tezFee, null), 5000)
+      // Usually a bad request will fail instantly, but we timeout all requests as a precaution.
+      await timeout(eztz.rpc.transfer(keys.pkh, keys, options.toPKH, tezAmount, tezFee, null),
+        options.timeout * 1000)
       console.log(colors.green('Transfer successfully injected.'))
     } catch (error) {
       console.error(colors.red('An error occurred. Error info below:'))
       // Catch can really catch any type, although typically Error/String.
       if (error instanceof Error && error.message) console.error(error.message)
       else if (!(error instanceof Error)) console.error(error)
-      else console.error('Unknown error (check your URI/connection).')
+      else console.error('Unknown error (check the URI and connection).')
       process.exit(1)
     }
   })
