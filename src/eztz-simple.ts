bc36be700e66ce5de2169f5522ce3d1f80369871
Sanjay Kannan <whaatt@gmail.com>
2018-11-28 15:56:12 -0500

    Make transfers batch.

--- src/eztz-simple.ts
+++ src/eztz-simple.ts
@@ -11,12 +11,10 @@ function die (message: string): void {
   console.error(colors.cyan(message))
   process.exit(1)
 }
-
-interface ForgeTransferCommand {
+interface ForgeBatchTransferCommand {
   node: string
   fromSK: eztz.SecretKey,
-  toPKH: eztz.PublicKeyHash,
-  amount: eztz.MuTez,
+  recipient: [[eztz.PublicKeyHash, eztz.MuTez] | undefined],
   fee: eztz.MuTez
   timeout: number
 }
@@ -44,20 +42,45 @@ program
     process.exit(1)
   })
 
+function collectRecipient (
+  value: string,
+  collector: Array<[eztz.PublicKeyHash, eztz.MuTez] | undefined>
+): Array<[eztz.PublicKeyHash, eztz.MuTez] | undefined> {
+  const tuple = value.split('@')
+  if (tuple.length !== 2) collector.push(undefined)
+  else collector.push(tuple as [eztz.PublicKeyHash, eztz.MuTez])
+  return collector
+}
+
+function makeTransfer (
+  toPKH: eztz.PublicKeyHash,
+  amount: eztz.MuTez,
+  fee: eztz.MuTez
+): eztz.TransferOperation {
+  return {
+    kind: 'transaction',
+    fee,
+    gas_limit: '200', // Default from EZTZ.
+    storage_limit: '0', // Default from EZTZ.
+    amount,
+    destination: toPKH
+  }
+}
+
 program
-  .command('forgeTransfer')
-  .description('forge a transfer between accounts')
+  .command('forgeBatchTransfer')
+  .description('forge a batch transfer to many recipients')
   .option('-n, --node <URI>', 'Tezos node URI')
   .option('-f, --fromSK <key>', 'Sender secret key')
-  .option('-t, --toPKH <hash>', 'Receiver public key hash')
-  .option('-a, --amount <value>', 'Amount to transfer in mutez')
-  .option('-p, --fee <value>', 'Desired operation fee in mutez')
+  .option('-r, --recipient <hash>@<mutez>', 'A recipient in the batch', collectRecipient, [])
+  .option('-p, --fee <mutez>', 'Operation fee per recipient in mutez')
   .option('-m, --timeout [sec]', 'Timeout in seconds for request', parseInt)
-  .action(async (options: ForgeTransferCommand) => {
+  .action(async (options: ForgeBatchTransferCommand) => {
     if (!options.node) die('No Tezos node provided.')
     if (!options.fromSK) die('No sender SK provided.')
-    if (!options.toPKH) die('No receiver PKH provided.')
-    if (!options.amount) die('No amount provided.')
+    if (options.recipient.length <= 0) die('No recipients provided.')
+    if (options.recipient.indexOf(undefined) !== -1) die('Malformed recipient(s).')
+    const recipients = options.recipient as [[eztz.PublicKeyHash, eztz.MuTez]]
     if (!options.fee) die('No fee provided.')
     if (!options.timeout) {
       console.log('Using default timeout (5 seconds).')
@@ -68,13 +91,13 @@ program
       const keys = eztz.crypto.extractKeys(options.fromSK)
       const secretKey = keys.sk as eztz.SecretKey
       keys.sk = false // Short-circuits the injection after forge.
-      const tezAmount = eztz.utility.totez(options.amount)
-      const tezFee = eztz.utility.totez(options.fee)
+      const transferOps = recipients.map(([toPKH, amount]) =>
+        makeTransfer(toPKH, amount, options.fee))
       // Usually a bad request will fail instantly, but we timeout all requests as a precaution.
       const forgeResult = await timeout(
-        eztz.rpc.transfer(keys.pkh, keys, options.toPKH, tezAmount, tezFee, null),
+        eztz.rpc.sendOperation(keys.pkh, transferOps, keys),
         options.timeout * 1000
-      ) as eztz.rpc.ForgedTransferResult
+      ) as eztz.ForgedTransferResult
       let signResult = eztz.crypto.sign(forgeResult.opbytes, secretKey, eztz.watermark.generic)
       // TODO: Pre-validate forged operation (will require some modification to EZTZ).
       console.log(colors.green('Transfer forged with the following signed contents:'))
