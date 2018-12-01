42cbccc1f6903f74eeb9341053bc9342820ea444
Sanjay Kannan <whaatt@gmail.com>
2018-11-30 19:17:31 -0500

    Improve my business logic.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # eztz-simple
-A simple CLI for sending Tezos transactions.
+A simple CLI for sending Tezos transactions. Successful commands will return their result as the
+last line(s) of the output, while errors will return their _kind_ as the last word of the output.
--- src/eztz-simple.ts
+++ src/eztz-simple.ts
@@ -7,10 +7,6 @@ import * as program from 'commander'
 import { eztz } from 'eztz.js'
 import { timeout } from 'promise-timeout'
 
-function die (message: string): void {
-  console.error(colors.cyan(message))
-  process.exit(1)
-}
 interface ForgeBatchTransferCommand {
   node: string
   fromSK: eztz.SecretKey,
@@ -20,17 +16,42 @@ interface ForgeBatchTransferCommand {
 }
 
 interface ExtractCommand {
-  node: string
-  secret: eztz.SecretKey,
+  secret: eztz.SecretKey
 }
 
+interface InjectCommand {
+  node: string,
+  object: string,
+  signed: string
+  timeout: number
+}
+
+// Prints an error to console. For programmatic use of the CLI, the last word of error output
+// contains an error kind value (either fatal, timeout, or connection). If callers wish to retry CLI
+// commands, they should only do so if the error kind is NOT fatal.
 function handleError (error: any): void {
+  const errorKind = (kind: string) => colors.cyan('Error Kind:') + ' ' + kind
   console.error(colors.red('An error occurred. Error info below:'))
-  // Catch can really catch any type, although typically Error/String.
-  if (error instanceof Error && error.message) console.error(error.message)
-  else if (!(error instanceof Error) && error !== '') console.error(error)
-  else console.error('Unknown error (check the URI and connection).')
+  if (error !== '') {
+    console.error(colors.gray(error))
+    if (error.message === 'Timeout') console.error(errorKind('timeout'))
+    else console.error(errorKind('fatal'))
+  } else {
+    console.error(colors.gray('Probable connection error (verify URI).'))
+    console.error(errorKind('connection'))
+  }
   process.exit(1)
+}
+
+function tryRunning (action: (...args: any[]) => Promise<any>):
+  (...args: any[]) => Promise<any> {
+  return async (...args) => {
+    try {
+      await action(...args)
+    } catch (error) {
+      handleError(error)
+    }
+  }
 }
 
 program
@@ -52,7 +73,7 @@ function collectRecipient (
   return collector
 }
 
-function makeTransfer (
+function buildTransferOp (
   toPKH: eztz.PublicKeyHash,
   amount: eztz.MuTez,
   fee: eztz.MuTez
@@ -74,57 +95,70 @@ program
   .option('-f, --fromSK <key>', 'Sender secret key')
   .option('-r, --recipient <hash>@<mutez>', 'A recipient in the batch', collectRecipient, [])
   .option('-p, --fee <mutez>', 'Operation fee per recipient in mutez')
-  .option('-m, --timeout [sec]', 'Timeout in seconds for request', parseInt)
-  .action(async (options: ForgeBatchTransferCommand) => {
-    if (!options.node) die('No Tezos node provided.')
-    if (!options.fromSK) die('No sender SK provided.')
-    if (options.recipient.length <= 0) die('No recipients provided.')
-    if (options.recipient.indexOf(undefined) !== -1) die('Malformed recipient(s).')
+  .option('-m, --timeout <sec>', 'Timeout in seconds for request', parseInt)
+  .action(tryRunning(async (options: ForgeBatchTransferCommand) => {
+    if (!options.node) throw new Error('No Tezos node provided.')
+    if (!options.fromSK) throw new Error('No sender SK provided.')
+    if (options.recipient.length <= 0) throw new Error('No recipients provided.')
+    if (options.recipient.indexOf(undefined) !== -1) throw new Error('Malformed recipient(s).')
     const recipients = options.recipient as [[eztz.PublicKeyHash, eztz.MuTez]]
-    if (!options.fee) die('No fee provided.')
-    if (!options.timeout) {
-      console.log('Using default timeout (5 seconds).')
-      options.timeout = 5
-    }
-    try {
-      eztz.node.setProvider(options.node)
-      const keys = eztz.crypto.extractKeys(options.fromSK)
-      const secretKey = keys.sk as eztz.SecretKey
-      keys.sk = false // Short-circuits the injection after forge.
-      const transferOps = recipients.map(([toPKH, amount]) =>
-        makeTransfer(toPKH, amount, options.fee))
-      // Usually a bad request will fail instantly, but we timeout all requests as a precaution.
-      const forgeResult = await timeout(
-        eztz.rpc.sendOperation(keys.pkh, transferOps, keys),
-        options.timeout * 1000
-      ) as eztz.ForgedTransferResult
-      let signResult = eztz.crypto.sign(forgeResult.opbytes, secretKey, eztz.watermark.generic)
-      // TODO: Pre-validate forged operation (will require some modification to EZTZ).
-      console.log(colors.green('Transfer forged with the following signed contents:'))
-      console.log(signResult.sbytes)
-    } catch (error) {
-      handleError(error)
-    }
-  })
+    if (!options.fee) throw new Error('No fee provided.')
+    if (!options.timeout) throw new Error('No timeout provided.')
+
+    eztz.node.setProvider(options.node)
+    const keys = eztz.crypto.extractKeys(options.fromSK)
+    const secretKey = keys.sk as eztz.SecretKey
+    keys.sk = false // Short-circuits the injection after forge.
+    const transferOps = recipients.map(([toPKH, amount]) =>
+      buildTransferOp(toPKH, amount, options.fee))
+    // Usually a bad request will fail instantly, but we timeout all requests as a precaution.
+    const forgeResult = await timeout(
+      eztz.rpc.sendOperation(keys.pkh, transferOps, keys),
+      options.timeout * 1000
+    ) as eztz.ForgeResult
+    let signResult = eztz.crypto.sign(forgeResult.opbytes, secretKey, eztz.watermark.generic)
+    forgeResult.opOb.signature = signResult.edsig
+
+    console.log(colors.green('Transfer forged with the following bytes and object:'))
+    console.log(signResult.sbytes)
+    console.log(JSON.stringify(forgeResult.opOb))
+  }))
 
 program
   .command('extract')
   .description('get the public key hash for a secret')
-  .option('-n, --node <URI>', 'Tezos node URI')
   .option('-s, --secret <key>', 'Secret key')
-  .action(async (options: ExtractCommand) => {
-    if (!options.node) die('No Tezos node provided.')
-    if (!options.secret) die('No secret key provided.')
-    try {
-      eztz.node.setProvider(options.node)
-      const keys = eztz.crypto.extractKeys(options.secret)
-      if (!keys.pkh) throw new Error('Extraction failed on a bad secret key.')
-      console.log(colors.green('Extracted the following public key hash:'))
-      console.log(keys.pkh)
-    } catch (error) {
-      handleError(error)
-    }
-  })
+  .action(tryRunning(async (options: ExtractCommand) => {
+    if (!options.secret) throw new Error('No secret key provided.')
+    const keys = eztz.crypto.extractKeys(options.secret)
+    if (!keys.pkh) throw new Error('Extraction failed on a bad secret key.')
+    console.log(colors.green('Extracted the following public key hash:'))
+    console.log(keys.pkh)
+  }))
+
+program
+  .command('inject')
+  .description('pre-validate and inject an operation')
+  .option('-n, --node <URI>', 'Tezos node URI')
+  .option('-s, --signed <bytes>', 'Signed operation bytes')
+  .option('-o, --object <JSON>', 'Serialized operation object')
+  .option('-m, --timeout <sec>', 'Timeout in seconds for request', parseInt)
+  .action(tryRunning(async (options: InjectCommand) => {
+    if (!options.node) throw new Error('No Tezos node provided.')
+    if (!options.signed) throw new Error('No signed operation bytes provided.')
+    if (!options.object) throw new Error('No operation object provided.')
+    if (!options.timeout) throw new Error('No timeout provided.')
+
+    eztz.node.setProvider(options.node)
+    const object = JSON.parse(options.object)
+    const injectResult = await timeout(
+      eztz.rpc.inject(object, options.signed),
+      options.timeout * 1000
+    ) as eztz.InjectResult
+
+    console.log(colors.green('Injected operation with hash:'))
+    console.log(injectResult.hash)
+  }))
 
 program
   .parse(process.argv)
